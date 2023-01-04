9ee81c7e0c43ba055676af9c9583504672ee879d
league <league@imac-109.attlocal.net>
2023-01-04 12:55:26 -0800

    I worked on robot square and mustache man and logical  DOTTTTTTTTTT

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,26 +17,26 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
-
+    	rob.penDown();
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed(<speed>);    **/
- 
+    	rob.setSpeed(50);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
-    	
+    	int count = 0;
 
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move(<distance>);    **/
-  
+    	rob.move(900);
 
 	// TURN.  Turn 90 degrees. Use this command:
     		  /**     rob.turn(<angle>);    **/
-
+    	rob.turn(90);
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
     		  /**     count += 1;    **/
-
+    		count+= 1;
     	
     	// Run the program. Did the Robot draw a line and turn?
     	 
@@ -52,9 +52,15 @@ public class RobotSquare {
     	 *      }
     	 **/
     	
-    	
+    		 while ( count < 4 ) {
+    			 rob.move(900); 
+    			 rob.turn(90);
+
+    			 count+= 1;
+    		}
     	// Run the program again. Did your Robot draw a square?
 
-
     }
+    
+
 }
