6bd858ad1aaa8a5c499ba72bb35fc62da769587d
league <league@imac-109.attlocal.net>
2023-01-06 12:48:47 -0800

    i made trivia

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String canu  = JOptionPane.showInputDialog("Can you Write code");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(canu.equalsIgnoreCase("yes")) {
+			 JOptionPane.showMessageDialog(null,"YOU WILL RULE THE WORLD MWAHAHAHAHAHAHHAHAHHAHAHHHAHAHAHHAHAHAHHAH!!!!!!!!!!!!!!!!!!");
+		}else {
+		 JOptionPane.showMessageDialog(null,"Sign up for classes at the league and go back to kindergarten!!!!!!!!!!!!!!!");
+		}		
+		
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,10 +1,22 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		String quest  = JOptionPane.showInputDialog("Are you smart");
+		// 2.  Ask the user a question 
+		int count = 0;
+		// 3.  Use an if statement to check if their answer is correct
+		if(quest.equalsIgnoreCase("yes")) { 
+			count-= 1;
+			JOptionPane.showMessageDialog(null,"sure what ever u say");
+		}else{ 
+			count+= 1;
+			JOptionPane.showMessageDialog(null,"nice job ur wrong ");
+		}
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
@@ -15,8 +27,22 @@ public class QuizGame {
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
+		String quest2  = JOptionPane.showInputDialog("what was the first basketball hoop made out of");
+		if(quest2.equalsIgnoreCase("A peach basket from georgia")) { 
+			count-= 1;
+			JOptionPane.showMessageDialog(null,"Amazing how did you know that");
+		}else{ 
+			count+= 1;
+			JOptionPane.showMessageDialog(null,"nice job ur wrong");
 		
-		
+		String quest3  = JOptionPane.showInputDialog("How many moon does jupiter have");
+		if(quest.equalsIgnoreCase("80")) { 
+			count-= 1;
+			JOptionPane.showMessageDialog(null,"Amazing how did you know that");
+		}else{ 
+			count+= 1;
+			JOptionPane.showMessageDialog(null,"nice job ur wrong ");
+		}}
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
